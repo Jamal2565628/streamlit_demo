912327e886958ee167c40c7113302fc84bbf6b2e
Jamal2565628 <41193793+Jamal2565628@users.noreply.github.com>
2024-12-15 20:57:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,11 @@ from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+authenticator = Authenticate(config['credentials'],
+                             config['cookie']['name'],
+                             config['cookie']['key'],
+                             config['cookie']['expiry_days'],
+                             config['preauthorized']
 )
 
 name, authentication_status, username = authenticator.login('Login', 'main')
